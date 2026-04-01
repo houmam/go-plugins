d74c5ef22d11a1cd1739bcb93945c02c1e855b7d
David Said <houmam@houmam.com>
2026-04-01 16:14:40 +1000

    Simplify connection status check in Connect method

--- broker/nats/nats.go
+++ broker/nats/nats.go
@@ -91,11 +91,8 @@ func (n *nbroker) Connect() error {
 	n.Lock()
 	defer n.Unlock()
 
-	if n.conn != nil {
-		switch n.conn.Status() {
-		case nats.CONNECTED, nats.RECONNECTING, nats.DISCONNECTED, nats.DRAINING_SUBS, nats.DRAINING_PUBS:
-			return nil
-		}
+	if n.conn != nil && !n.conn.IsClosed() {
+		return nil
 	}
 
 	opts := n.nopts
